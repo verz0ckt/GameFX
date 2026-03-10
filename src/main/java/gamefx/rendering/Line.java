ec14cba5697a6ebda17880810a814cf8b7c00821
verz0ckt <eisele-jonas@web.de>
2026-03-10 12:57:37 +0100

    WIP!!! Started buffer drawing

--- src/main/java/gamefx/rendering/Line.java
+++ src/main/java/gamefx/rendering/Line.java
@@ -37,40 +37,94 @@ public class Line extends Drawable
             x2 = alt[0];
             y2 = alt[1];
         }
+        int dx = (int) Math.abs((int)x2-(int)x1)<<2;
+        int dy = (int) Math.abs((int)y2-(int)y1)<<2;
+
+        if(dx>dy){
+
+            int x = ((int) x1)<<2;
+            int xEnd = ((int) x2)<<2;
+            int y = ((int) y1)<<2;
+            int signY = (int) Math.signum(y2-y1)*4;
+            if(x1 > x2){
+                x = ((int) x2)<<2;
+                xEnd = ((int) x1)<<2;
+                y = ((int) y2)<<2;
+                signY *=-1;
+            }
+            dy <<= 1;
+            int error = dy-dx;
+            dx = dx<<1;
+            /*
+            if(x >= ren.maxWidth && x < 0 && y >= ren.maxHeight && y < 0) {
+                Color col = (Color) paint;
+                int pos = (x + y * ren.maxWidth) * 4;
+                buffer.put(pos++, (byte) (col.getBlue() * 255));
+                buffer.put(pos++, (byte) (col.getGreen() * 255));
+                buffer.put(pos++, (byte) (col.getRed() * 255));
+                buffer.put(pos, (byte) (col.getOpacity() * 255));
+            }*/
+            while(x <= xEnd){
+                Color col = (Color) paint;
+                x += 4;
+                error += dy;
+                if(error > 0){
+                    error -= dx;
+                    y += signY;
+                }
+                if(x >= ren.maxWidth || x < 0 || y >= ren.maxHeight || y < 0) System.out.println("g");
+                int pos = x+y*ren.maxWidth;
+                buffer.put(pos++,(byte) (col.getBlue()*255));
+                buffer.put(pos++,(byte) (col.getGreen()*255));
+                buffer.put(pos++,(byte) (col.getRed()*255));
+                buffer.put(pos,(byte) (col.getOpacity()*255));
+            }
+        }else{
+            //add shifting
+            int x = (int) x1;
+            int y = (int) y1;
+            int yEnd = (int) y2;
+            int signX = (int) Math.signum(x2-x1);
+            if(y1 > y2){
+                x = (int) x2;
+                y = (int) y2;
+                yEnd = (int) y1;
+                signX *=-1;
+            }
+            dx = dx<<1;
+            int error = dx-dy;
+            dy = dy<<1;
+            if(x >= ren.maxWidth && x < 0 && y >= ren.maxHeight && y < 0) {
+                Color col = (Color) paint;
+                int pos = (x + y * ren.maxWidth) * 4;
+                buffer.put(pos++, (byte) (col.getBlue() * 255));
+                buffer.put(pos++, (byte) (col.getGreen() * 255));
+                buffer.put(pos++, (byte) (col.getRed() * 255));
+                buffer.put(pos, (byte) (col.getOpacity() * 255));
+            }
+            while(y <= yEnd){
+                Color col = (Color) paint;
+                y += 1;
+                error += dx;
+                if(error > 0){
+                    error -= dy;
+                    x += signX;
+                }
+                if(x >= ren.maxWidth || x < 0 || y >= ren.maxHeight || y < 0) continue;
+                int pos = (x+y*ren.maxWidth)*4;
+                buffer.put(pos++,(byte) (col.getBlue()*255));
+                buffer.put(pos++,(byte) (col.getGreen()*255));
+                buffer.put(pos++,(byte) (col.getRed()*255));
+                buffer.put(pos,(byte) (col.getOpacity()*255));
+            }
+        }
 
         //gc.setStroke(paint);
         //gc.strokeLine(x1,y1,x2,y2);
     }
-    private void drawLine(int v1,int u1,int v2,int u2,ByteBuffer buffer){
-        int dV = Math.abs(v2-v1);
-        int dU = Math.abs(u2-u1);
-        int v = (int) v1;//v1
-        int u = (int) u1;//v2
-        if(v1> v2){
-            v = (int) v2;
-            u = (int) u2;
-        }
-        //fix direction
-        double m = 0;
-        if(dV != 0){
-            m = (double) dU / dV;
-        }
+    public static void drawLine(ByteBuffer buffer,int x1,int dx,int dy, int y1, int x2, int y2, Color paint){
 
-        double error = 0;
-        for(int i = 0; i< dV; i++){
-            Color col = (Color)paint;
-            v += 1;
-            error += m;
-            if(error >= 1){
-                error--;
-                u +=1;
-            }
-            int pos = (v+u*ren.maxWidth)*4;
-            buffer.put(pos++,(byte) (col.getBlue()*255));
-            buffer.put(pos++,(byte) (col.getGreen()*255));
-            buffer.put(pos++,(byte) (col.getRed()*255));
-            buffer.put(pos,(byte) (col.getOpacity()*255));
-        }
+
     }
 
 }
